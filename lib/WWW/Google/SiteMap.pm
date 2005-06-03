420b69d9404e2cdf35421dec06de37d4af4fa335
Jason Kohles <cpan@jasonkohles.com>
2005-06-03 21:11:47 +0000

    Import of JASONK/WWW-Google-SiteMap-0.02 from CPAN.
    gitpan-cpan-distribution: WWW-Google-SiteMap
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         JASONK/WWW-Google-SiteMap-0.02.tar.gz
    gitpan-cpan-author:       JASONK
    gitpan-cpan-maturity:     released

---
+++ lib/WWW/Google/SiteMap.pm
@@ -0,0 +1,295 @@
+package WWW::Google::SiteMap;
+our $VERSION = '0.02';
+
+=head1 NAME
+
+WWW::Google::SiteMap - Perl extension for managing Google SiteMaps
+
+=head1 SYNOPSIS
+
+  use WWW::Google::SiteMap;
+
+  my $map = WWW::Google::SiteMap->new(file => 'sitemap.gz');
+
+  # Main page, changes a lot because of the blog
+  $map->add(WWW::Google::SiteMap::URL->new(
+    loc        => 'http://www.jasonkohles.com/',
+    lastmod    => '2005-06-03',
+    changefreq => 'daily',
+    priority   => 1.0,
+  ));
+
+  # Top level directories, don't change as much, and have a lower priority
+  $map->add({
+    loc        => "http://www.jasonkohles.com/$_/",
+    changefreq => 'weekly',
+    priority   => 0.9, # lower priority than the home page
+  ) for qw(
+    software gpg hamradio photos scuba snippets tools
+  );
+
+  $map->write;
+
+=head1 DESCRIPTION
+
+The Sitemap Protocol allows you to inform search engine crawlers about URLs
+on your Web sites that are available for crawling. A Sitemap consists of a
+list of URLs and may also contain additional information about those URLs,
+such as when they were last modified, how frequently they change, etc.
+
+This module allows you to create and modify sitemaps.
+
+=head1 METHODS
+
+=over 4
+
+=cut
+
+use strict;
+use warnings;
+use WWW::Google::SiteMap::URL;
+require XML::Simple;
+require IO::Zlib;
+require IO::File;
+require UNIVERSAL;
+use Carp qw(carp croak);
+
+=item new()
+
+Creates a new WWW::Google::SiteMap object.
+
+  my $map = WWW::Google::SiteMap->new(
+    file	=> 'sitemap.gz',
+  );
+
+=cut
+
+sub new {
+	my $class = shift;
+	my %opts = @_;
+	my $self = bless({}, $class);
+	while(my($key,$value) = each %opts) { $self->$key($value) }
+	if($self->file && -e $self->file) { $self->read }
+	return $self;
+}
+
+=item read()
+
+Read a sitemap in to this object.  If a filename is specified, it will be
+read from that file, otherwise it will be read from the file that was
+specified with the file() method.  Reading of compressed files is done
+automatically if the filename ends with .gz.
+
+=cut
+
+sub read {
+	my $self = shift;
+	my $file = shift || $self->file ||
+		croak "No filename specified for WWW::Google::SiteMap::read";
+
+	my $fh;
+	if($file =~ /\.gz$/i) {
+		$fh = IO::Zlib->new($file,"rb");
+	} else {
+		$fh = IO::File->new($file,"r");
+	}
+	# I think there is a strange bug interaction between IO::Zlib and
+	# XML::Simple, just passing the filehandle to XMLin doesn't work
+	my $tmp = XML::Simple::XMLin(join('',$fh->getlines),
+		ForceArray	=> [qw(url)],
+	);
+	$self->xmlns($tmp->{xmlns}) if $tmp->{xmlns};
+	$self->urls(map {
+		WWW::Google::SiteMap::URL->new(%{$_}, lenient => 1)
+	} @{$tmp->{url}});
+}
+
+=item write([$file])
+
+Write the sitemap out to the file.  If a filename is specified, it will be
+written to that file, otherwise it will be written to the file that was
+specified with the file() method.  Writing of compressed files is done
+automatically if the filename ends with .gz.
+
+=cut
+
+sub write {
+	my $self = shift;
+	my $file = shift || $self->file ||
+		croak "No filename specified for WWW::Google::SiteMap::write";
+
+	my $fh;
+	if($file =~ /\.gz$/i) {
+		$fh = IO::Zlib->new($file,"wb9");
+	} else {
+		$fh = IO::File->new($file,"w");
+	}
+	$fh->print($self->xml);
+}
+
+=item urls()
+
+Return the L<WWW::Google::SiteMap::URL> objects that make up the sitemap.
+
+=cut
+
+sub urls {
+	my $self = shift;
+	$self->{urls} = \@_ if @_;
+	my @urls = grep { ref($_) && defined $_->loc } @{$self->{urls}};
+	return wantarray ? @urls : \@urls;
+}
+
+=item add(item,[item...])
+
+Add the L<WWW::Google::SiteMap::URL> items listed to the sitemap.
+
+If you pass hashrefs instead of L<WWW::Google::SiteMap::URL> objects, it
+will turn them into objects for you.  If the first item you pass is a
+simple scalar that matches \w, it will assume that the values passed are
+a hash for a single object.  If the first item passed matches m{^\w+://}
+(i.e. it looks like a URL) then all the arguments will be treated as URLs,
+and L<WWW::Google::SiteMap::URL> objects will be constructed for them, but only
+the loc field will be populated.
+
+This means you can do any of these:
+
+  # create the WWW::Google::SiteMap::URL object yourself
+  my $url = WWW::Google::SiteMap::URL->new(
+    loc => 'http://www.jasonkohles.com/',
+    priority => 1.0,
+  );
+  $map->add($url);
+
+  # or
+  $map->add(
+    { loc => 'http://www.jasonkohles.com/' },
+    { loc => 'http://www.jasonkohles.com/software/google-sitemap/' },
+    { loc => 'http://www.jasonkohles.com/software/geo-shapefile/' },
+  );
+
+  # or
+  $map->add(
+    loc       => 'http://www.jasonkohles.com/',
+    priority  => 1.0,
+  );
+
+  # or even something funkier
+  $map->add(qw(
+    http://www.jasonkohles.com/
+    http://www.jasonkohles.com/software/www-google-sitemap/
+    http://www.jasonkohles.com/software/geo-shapefile/
+    http://www.jasonkohles.com/software/text-fakedata/
+  ));
+  foreach my $url ($map->urls) { $url->changefreq('daily') }
+    
+=cut
+
+sub add {
+	my $self = shift;
+	if(ref($_[0])) {
+		if(UNIVERSAL::isa($_[0],"WWW::Google::SiteMap::URL")) {
+			push(@{$self->{urls}}, @_);
+		} elsif(ref($_[0]) =~ /HASH/) {
+			push(@{$self->{urls}},map {
+				WWW::Google::SiteMap::URL->new($_)
+			} @_);
+		}
+	} elsif($_[0] =~ /^\w+$/) {
+		push(@{$self->{urls}}, WWW::Google::SiteMap::URL->new(@_));
+	} elsif($_[0] =~ m{^\w+://}) {
+		push(@{$self->{urls}}, map {
+			WWW::Google::SiteMap::URL->new(loc => $_)
+		} @_);
+	} else {
+		croak "Can't turn '".(
+			ref($_[0]) || $_[0]
+		)."' into WWW::Google::SiteMap::URL object";
+	}
+}
+
+=item xml()
+
+Return the xml representation of the sitemap
+
+=cut
+
+sub xml {
+	my $self = shift;
+
+	return XML::Simple::XMLout(
+		{
+			xmlns			=> $self->xmlns,
+			url				=> [map { $_->hash } $self->urls],
+		},
+		AttrIndent		=> $self->pretty,
+		NoIndent		=> !$self->pretty,
+		NoSort			=> !$self->pretty,
+		SuppressEmpty	=> 1,
+		RootName		=> 'urlset',
+		XMLDecl			=> '<?xml version="1.0" encoding="UTF-8"?>',
+	);
+}
+
+=item file()
+
+Get or set the filename associated with this object.  If you call read() or
+write() without a filename, this is the default.
+
+=cut
+
+sub file {
+	my $self = shift;
+	$self->{file} = shift if @_;
+	return $self->{file};
+}
+
+=item xmlns()
+
+Get or set the XML namespace to be used for the urlset.  Default is
+http://www.google.com/schemas/sitemap/0.84
+
+=cut
+
+sub xmlns {
+	my $self = shift;
+	$self->{xmlns} = shift if @_;
+	return $self->{xmlns} || 'http://www.google.com/schemas/sitemap/0.84';
+}
+
+=item pretty()
+
+Set this to a true value to enable 'pretty-printing' on the XML output.  If
+false (the default) the XML will be more compact but not as easily readable
+for humans (Google and other computers won't care what you set this to).
+
+=cut
+
+sub pretty {
+	my $self = shift;
+	$self->{pretty} = shift if @_;
+	return $self->{pretty};
+}
+
+=back
+
+=head1 SEE ALSO
+
+L<https://www.google.com/webmasters/sitemaps/docs/en/protocol.html>
+
+=head1 AUTHOR
+
+Jason Kohles, E<lt>email@jasonkohles.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2005 by Jason Kohles
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.4 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
+
+1;
+__END__
---
+++ lib/WWW/Google/SiteMap/URL.pm
@@ -0,0 +1,181 @@
+package WWW::Google::SiteMap::URL;
+our $VERSION = '0.02';
+
+=head1 NAME
+
+WWW::Google::SiteMap::URL - URL Helper class for WWW::Google::SiteMap
+
+=head1 SYNOPSIS
+
+  use WWW::Google::SiteMap;
+
+=head1 DESCRIPTION
+
+This is a helper class that supports L<WWW::Google::SiteMap>.
+
+=head1 METHODS
+
+=over 4
+
+=cut
+
+use strict;
+use warnings;
+use Carp qw(carp croak);
+
+=item new()
+
+=cut
+
+sub new {
+	my $class = shift;
+	my %opts = ref($_[0]) ? %{$_[0]} : @_;
+
+	my $self = bless({}, $class);
+	while(my($key,$value) = each %opts) { $self->$key($value) }
+	return $self;
+}
+
+=item loc()
+
+Change the URL associated with this object.
+
+=cut
+
+sub loc {
+	shift->_doval('loc', sub {
+		local $_ = shift;
+		return unless defined;
+		return 'must be less than 2048 characters long' unless length($_) < 2048;
+		return 'must be a fully qualified url' unless m{^https?://};
+		return;
+	}, @_);
+}
+
+=item changefreq()
+
+Set the change frequency of the object.
+
+=cut
+
+sub changefreq {
+	shift->_doval('changefreq', sub {
+		local $_ = shift;
+		my @values = qw(always hourly daily weekly monthly yearly never);
+		my $re = join('|',@values);
+		return unless defined;
+		return 'must be one of '.join(', ',@values) unless /^$re$/;
+		return;
+	}, @_);
+}
+
+=item lastmod()
+
+Set the last modified time.
+
+=cut
+
+sub lastmod {
+	shift->_doval('lastmod', sub {
+		local $_ = shift;
+		return unless defined;
+		return 'must be an ISO-8601 formatted date string' unless (
+			/^\d{4}-\d{2}-\d{2}(T\d\d:\d\d:\d\d\+\d\d:\d\d)?$/
+		);
+		return;
+	}, @_);
+}
+
+=item priority()
+
+Set the priority.
+
+=cut
+
+sub priority {
+	shift->_doval('priority', sub {
+		local $_ = shift;
+		return unless defined;
+		return 'must be a number' unless /^[\d\.]+$/;
+		return 'must be greater than 0.0' unless $_ >= 0.0;
+		return 'must be less than 1.0' unless $_ <= 1.0;
+		return;
+	}, @_);
+}
+
+sub _doval {
+	my $self = shift;
+	my $var = shift;
+	my $valid = shift;
+	return $self->{$var} unless @_;
+	my $value = shift;
+	if(my $res = $valid->($value)) {
+		my $msg = "'$value' is not a valid value for $var: $res";
+		if($self->{lenient}) { carp $msg } else { croak $msg }
+	} else {
+		$self->{$var} = $value;
+	}
+}
+
+=item delete()
+
+Delete this object from the sitemap.
+
+=cut
+
+sub delete {
+	my $self = shift;
+	for(keys %{$self}) { $self->{$_} = undef }
+}
+
+=item lenient()
+
+If lenient contains a true value, then errors will not be fatal.
+
+=cut
+
+sub lenient {
+	my $self = shift;
+	$self->{lenient} = shift if @_;
+	return $self->{lenient};
+}
+
+=item hash()
+
+Return a hashref of the data.  This is used internally by
+L<WWW::Google::SiteMap> to provide a data structure more suitable to
+passing to L<XML::Simple>.
+
+=cut
+
+sub hash {
+	my $self = shift;
+	my $hash = {%{$self}};
+	delete($hash->{$_}) for grep { ! $hash->{$_} } keys %{$hash};
+	return $hash;
+}
+
+=back
+
+=head1 SEE ALSO
+
+L<http://www.jasonkohles.com/software/www-google-sitemap/>
+
+L<https://www.google.com/webmasters/sitemaps/docs/en/protocol.html>
+
+=head1 AUTHOR
+
+Jason Kohles, E<lt>email@jasonkohles.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2005 by Jason Kohles
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.4 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
+
+1;
+__END__
